b782173c1e248792ff731ecf44a30fbc22915b85
Andrew Hung <druphoria@utexas.edu>
2018-02-03 18:53:55 -0800

    even more swirl stuff

---
+++ Assets/Source/Scripts/Background.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    void Awake ()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        transform.localScale = new Vector3(cameraSize.x / spriteSize.x, cameraSize.y / spriteSize.y, 1);
+    }
+}
