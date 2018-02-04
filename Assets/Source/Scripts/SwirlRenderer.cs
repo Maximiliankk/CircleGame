eb5b9dcae91a6cee6f53f62b61f8f4682faeaf46
Andrew Hung <druphoria@utexas.edu>
2018-02-03 17:28:43 -0800

    implemented basic swirl

---
+++ Assets/Source/Scripts/SwirlRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwirlRenderer : MonoBehaviour {
+
+    private Material mat;
+
+	void Awake()
+    {
+        mat = Resources.Load("SwirlMaterial") as Material;
+    }
+
+    void Update()
+    {
+        Vector2 pos = Camera.main.WorldToViewportPoint(transform.position);
+        mat.SetVector("_Center", new Vector4(pos.x, pos.y));
+    }
+}
