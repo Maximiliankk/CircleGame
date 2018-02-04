305cf72b94518207a80f1185fdb996f2bc3708a9
Andrew Hung <druphoria@utexas.edu>
2018-02-03 18:18:55 -0800

    more swirl stuff

--- Assets/Source/Scripts/SwirlRenderer.cs
+++ Assets/Source/Scripts/SwirlRenderer.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class SwirlRenderer : MonoBehaviour {
 
-    private Material mat;
+    public Material mat;
+    public CircleCollider2D circleCollider;
+    public float swirlPullMultiplier;
+    public float deltaAngularVelocity;
 
-	void Awake()
+    List<Rigidbody2D> affectedRigidbodies;
+
+    void Awake ()
     {
-        mat = Resources.Load("SwirlMaterial") as Material;
+        affectedRigidbodies = new List<Rigidbody2D>();
     }
 
     void Update()
     {
         Vector2 pos = Camera.main.WorldToViewportPoint(transform.position);
         mat.SetVector("_Center", new Vector4(pos.x, pos.y));
+
+        foreach (Rigidbody2D rb2d in affectedRigidbodies)
+        {
+            float dist = Vector2.Distance(transform.position, rb2d.transform.position);
+            rb2d.AddForce((transform.position - rb2d.transform.position).normalized * Mathf.Clamp01(1 - dist/circleCollider.radius) * swirlPullMultiplier);
+            rb2d.AddTorque(deltaAngularVelocity);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
+        if (rb2d && !affectedRigidbodies.Contains(rb2d))
+        {
+            affectedRigidbodies.Add(rb2d);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
+        if (rb2d && affectedRigidbodies.Contains(rb2d))
+        {
+            affectedRigidbodies.Remove(rb2d);
+        }
     }
 }
